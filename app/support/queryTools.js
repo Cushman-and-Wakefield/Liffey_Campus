736ae554d25ed38fb3fe49b0ec015ba626d5eb93
Cushman-and-Wakefield <97603362+Cushman-and-Wakefield@users.noreply.github.com>
2022-02-07 12:51:24 +0000

    Update queryTools.js

--- app/support/queryTools.js
+++ app/support/queryTools.js
@@ -52,7 +52,7 @@ define([
                     for (var i = 0; i < selection.length; i++) {
                         values.push(selection[i].attributes[fieldname]);
                     }
-
+                    console.info(values);
                     values.sort(function (a, b) { return a - b; });
 
                     for (var j = 0; j < values.length; j++) {
@@ -60,6 +60,7 @@ define([
                             values.splice(j, 1);
                         }
                     }
+                    console.info(values);
 
                     callback(values);
 
